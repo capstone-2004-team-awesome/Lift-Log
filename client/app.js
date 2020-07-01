1a3d6b52a98b371ff52a1f4570b11c22dc82022d
Connie Lok <conniehlok@gmail.com>
2020-07-01 16:20:55 -0400

    feat: add camera component

--- client/app.js
+++ client/app.js
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <div>
       <Navbar />
+
       <Routes />
     </div>
   )
---
+++ client/components/StartWorkout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Camera from './Camera'
+
+const StartWorkout = () => {
+  return (
+    <div>
+      <Camera />
+    </div>
+  )
+}
+
+export default StartWorkout
