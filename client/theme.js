bc670fdd083ee0c3413a8303bfe644992ae4e594
Connie Lok <conniehlok@gmail.com>
2020-07-04 00:39:29 -0400

    style: fix button styling

--- client/theme.js
+++ client/theme.js
@@ -2,9 +2,18 @@ import zIndex from '@material-ui/core/styles/zIndex'
 
 // TODO: fix button props because this messes up the display in the menu bar
 const themeObj = {
+  // can override the box shadow here and remove it so appbar buttons don't have it
+  // right now removing box-shadow is done in Navbar makeStyles
+  // overrides: {
+  //   MuiButton: {
+  //     root: {
+  //       boxShadow: '0px 0px'
+  //     }
+  //   }
+  // },
   props: {
     MuiButton: {
-      // variant: 'contained',
+      variant: 'contained',
       color: 'primary'
     }
   },
