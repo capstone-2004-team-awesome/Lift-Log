0dec987556daea1331d1f6e3a4787aa117e03f25
Irina Gabuaeva <irina@MacBook-Pro.fios-router.home>
2020-07-20 19:28:32 -0400

    login page styled

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -8,12 +8,15 @@ import {
   Typography,
   Button,
   CardContent,
-  Input
+  Input,
+  Divider,
+  sizing
 } from '@material-ui/core'
 
 /**
  * COMPONENT
  */
+
 const AuthForm = props => {
   const {name, displayName, handleSubmit, error} = props
 
@@ -23,15 +26,16 @@ const AuthForm = props => {
         container
         spacing={1}
         align="center"
+        alignContent="center"
         direction="column"
         justify="space-between"
       >
-        <Grid item xs={12}>
-          <Card>
+        <Grid item xs={6} md={6} lg={6}>
+          <Card width="60%">
             <CardContent>
               <Typography variant="h6">
                 Email
-                <Input name="email" type="text" />
+                <Input name="email" type="text" align="center" />
               </Typography>
             </CardContent>
             <CardContent>
@@ -40,12 +44,14 @@ const AuthForm = props => {
                 <Input name="password" type="password" />
               </Typography>
             </CardContent>
+            <CardContent>
+              <Grid item xs={12}>
+                <Button type="submit">{displayName}</Button>
+              </Grid>
+            </CardContent>
           </Card>
         </Grid>
-        <Grid item xs={12}>
-          <Button type="submit">{displayName}</Button>
-        </Grid>
-
+        <Divider />
         {error && error.response && <div> {error.response.data} </div>}
 
         <a href="/auth/google">{displayName} with Google</a>
