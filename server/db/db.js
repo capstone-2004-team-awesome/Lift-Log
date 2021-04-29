33746f5c9247c5db49470c9ce903d85387c374de
Irina Gabuaeva <irina@macbook-pro.lan>
2021-04-28 21:21:07 -0400

    modified db model by adding ssl and dialect in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -6,9 +6,9 @@ const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
-    logging: false
-  },
-  {
+    logging: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
     dialectOptions: {
       ssl: {
         rejectUnauthorized: false
@@ -16,6 +16,7 @@ const db = new Sequelize(
     }
   }
 )
+
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ if (process.env.NODE_ENV === 'test') {
  * keys as environment variables, so that they can still be read by the
  * Node process on process.env
  */
-if (process.env.NODE_ENV !== 'production') require('../secrets')
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 // passport registration
 passport.serializeUser((user, done) => done(null, user.id))
